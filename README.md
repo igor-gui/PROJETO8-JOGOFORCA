5e6b1ee5bf9b320721c7473727e0d68377af6a3b
Igor Guimarães <igorgui.tec@gmail.com>
2022-09-26 17:06:13 -0300

    Botões somente clicáveis uma vez

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ## JOGO DA FORCA - REACT.js
+<body style="display: flex; align-items: center">
 
+<p>
+O usuário acessa o jogo e pode pedir para o site escolher uma palavra aleatória, armazenada em um array. 
+O programa guarda a palavra em uma variável, mas renderiza um símbolo de underline ( _ ) no lugar de cada caracter, 
+até que o chute do usuário acerte a letra que deveria estar naquela posição.
+</p>
+
+<p>
+Ao clicar em uma letra, está deixa de ser uma opção de chute, ficando com o fundo escuro e não mais clicável. O caracter é comparado com os da string e, caso um ou mais deles sejam iguais ao chute do usuário, essa o programa renderiza essa letra, substituindo o traço underline que estiver na posição correspondente.
+</p>
+</body>
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import palavras from './palavras'
 
-const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z']
+const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 export default function App() {
     const [word, setWord] = useState('')
     let [wordToShow, setWordToShow] = useState('')
-    let [trialClass , setTrialClass] = useState("letra")
+    let [clicados, setClicados] = useState([])
+    const [erros, setErros] = useState(0)
 
     function pickWord() {
+        setClicados([])
+        setErros(0)
         const sort = Math.floor(Math.random() * palavras.length + 1)
 
         let trace = [];
@@ -22,32 +25,47 @@ export default function App() {
         setWord([...palavras[sort]]);
 
     }
-    function trial(char){
-        for(let i = 0; i < word.length; i++){
-            setTrialClass("letra clicada")
-            if(word[i] === char){
-                wordToShow[i] = char;
-            } 
-        }
-    }
+    function trial(char, i) {
+        for (let i = 0; i < word.length; i++) {
 
+            if (erros < 6) {
+
+                if (word[i] === char) {
+                    wordToShow[i] = char;
+                    setWordToShow([...wordToShow])
+                    setClicados([...clicados, char])
+                    let soma = erros;
+                    setErros(soma)
+
+                } else {
+                    setClicados([...clicados, char])
+                    let soma = erros + 1;
+                    setErros(soma)
+                }
+            } else {
+                setClicados([...letras])
+            }
+        }
+
+    }
 
 
 
     return (
         <>
             <main>
-                <img src='./assets/forca0.png' />
+                <img data-identifier="game-image" src={`./assets/forca${erros}.png`} />
                 <div className="palavra">
-                    <button onClick={pickWord}>Escolher Palavra</button>
-                    <h1>{wordToShow}</h1>
+                    <button data-identifier="choose-word" onClick={pickWord}>Escolher Palavra</button>
+                    <h1 data-identifier="word">{wordToShow}</h1>
                 </div>
 
             </main>
 
             <ul className='letras'>
-                {letras.map((e) => <li onClick={(e) => trial(e)} className={trialClass}>{e}</li>)}
+                {letras.map((e, i) => <li data-identifier="letter" onClick={() => trial(e, i)} className={clicados.includes(e) ? "letra clicada" : "letra"}>{e}</li>)}
             </ul>
+            <input data-identifier="type-guess"></input>
         </>
     )
 }
